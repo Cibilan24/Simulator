a6a300704da8e9fc1d6229fccb0625b71fa9341d
Cibilan24 <149468339+Cibilan24@users.noreply.github.com>
2025-06-12 12:11:31 +0530

    Create Simulator_Repositories.java

---
+++ Simulator_Repositories.java
@@ -0,0 +1,109 @@
+package com.hdsoft.hdpay.Repositories;
+
+public class Simulator001
+{
+	private String SUBORGCODE;
+	private String SYSCODE;
+	private String CHCODE;
+	private String SERVICECD;
+	private String SERVNAME;
+	private String FORMAT;
+	private String METHOD;
+	private String PROTOCOL;
+	private String CHTYPE;
+	private String URI;
+	private String PAYLOAD;
+	private String HEADERID;
+	private String SIMIDPATH;
+	
+	private String FLOW;
+	
+	
+	public String getSUBORGCODE() {
+		return SUBORGCODE;
+	}
+	public void setSUBORGCODE(String sUBORGCODE) {
+		SUBORGCODE = sUBORGCODE;
+	}
+	public String getSYSCODE() {
+		return SYSCODE;
+	}
+	public void setSYSCODE(String sYSCODE) {
+		SYSCODE = sYSCODE;
+	}
+	public String getCHCODE() {
+		return CHCODE;
+	}
+	public void setCHCODE(String cHCODE) {
+		CHCODE = cHCODE;
+	}
+	public String getSERVICECD() {
+		return SERVICECD;
+	}
+	public void setSERVICECD(String sERVICECD) {
+		SERVICECD = sERVICECD;
+	}
+	public String getSERVNAME() {
+		return SERVNAME;
+	}
+	public void setSERVNAME(String sERVNAME) {
+		SERVNAME = sERVNAME;
+	}
+	public String getFORMAT() {
+		return FORMAT;
+	}
+	public void setFORMAT(String fORMAT) {
+		FORMAT = fORMAT;
+	}
+	public String getMETHOD() {
+		return METHOD;
+	}
+	public void setMETHOD(String mETHOD) {
+		METHOD = mETHOD;
+	}
+	public String getPROTOCOL() {
+		return PROTOCOL;
+	}
+	public void setPROTOCOL(String pROTOCOL) {
+		PROTOCOL = pROTOCOL;
+	}
+	public String getCHTYPE() {
+		return CHTYPE;
+	}
+	public void setCHTYPE(String cHTYPE) {
+		CHTYPE = cHTYPE;
+	}
+	public String getURI() {
+		return URI;
+	}
+	public void setURI(String uRI) {
+		URI = uRI;
+	}
+	public String getPAYLOAD() {
+		return PAYLOAD;
+	}
+	public void setPAYLOAD(String pAYLOAD) {
+		PAYLOAD = pAYLOAD;
+	}
+	public String getHEADERID() {
+		return HEADERID;
+	}
+	public void setHEADERID(String hEADERID) {
+		HEADERID = hEADERID;
+	}
+	public String getSIMIDPATH() {
+		return SIMIDPATH;
+	}
+	public void setSIMIDPATH(String sIMIDPATH) {
+		SIMIDPATH = sIMIDPATH;
+	}
+	
+	public String getFLOW() {
+		return FLOW;
+	}
+	public void setFLOW(String fLOW) {
+		FLOW = fLOW;
+	}
+	
+	
+}
